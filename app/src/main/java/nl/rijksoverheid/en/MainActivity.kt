c58e5d5c08229d897e6d78e60a1030c9161d80d0
Jolanda Verhoef <jolanda.verhoef@gmail.com>
2020-05-28 20:47:28 +0200

    Add basic Splash screen

--- app/src/main/java/nl/rijksoverheid/en/MainActivity.kt
+++ app/src/main/java/nl/rijksoverheid/en/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
