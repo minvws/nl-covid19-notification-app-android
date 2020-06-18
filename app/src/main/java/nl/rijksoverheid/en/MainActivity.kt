e01f68fcd0b91b3f7c19c5e73111e17e0be8c056
Jolanda Verhoef <jolanda.verhoef@bdmcc.net>
2020-06-18 21:08:15 +0200

    Draw content behind status bar

--- app/src/main/java/nl/rijksoverheid/en/MainActivity.kt
+++ app/src/main/java/nl/rijksoverheid/en/MainActivity.kt
@@ -9,6 +9,7 @@ package nl.rijksoverheid.en
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -24,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
         viewModel.notificationsResult.observe(this, EventObserver {
             when (it) {
                 is ExposureNotificationsViewModel.NotificationsStatusResult.ConsentRequired -> {
