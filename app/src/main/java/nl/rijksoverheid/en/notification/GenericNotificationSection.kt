32099bb1c72cb3bb7c2c729ecfe18fbc5c48482f
Jolanda Verhoef <jolanda.verhoef@bdmcc.net>
2020-07-16 10:30:59 +0200

    Update post-notification and generic notification content based on design v0.8

--- app/src/main/java/nl/rijksoverheid/en/notification/GenericNotificationSection.kt
+++ app/src/main/java/nl/rijksoverheid/en/notification/GenericNotificationSection.kt
@@ -9,25 +9,24 @@ package nl.rijksoverheid.en.notification
 import com.xwray.groupie.Section
 import nl.rijksoverheid.en.R
 import nl.rijksoverheid.en.items.BulletedListItem
-import nl.rijksoverheid.en.items.ButtonItem
 import nl.rijksoverheid.en.items.HeaderItem
 import nl.rijksoverheid.en.items.IllustrationItem
 import nl.rijksoverheid.en.items.MessageBoxItem
 import nl.rijksoverheid.en.items.ParagraphItem
 
-class GenericNotificationSection(phoneNumber: String, onCallClicked: () -> Unit) : Section(
+class GenericNotificationSection(phoneNumber: String) : Section(
     listOf(
         IllustrationItem(R.drawable.illustration_generic_notification),
         HeaderItem(R.string.generic_notification_header_1),
         ParagraphItem(R.string.generic_notification_paragraph_2),
-        HeaderItem(R.string.treat_perspective_header_1),
-        BulletedListItem(R.string.treat_perspective_list_2),
-        ParagraphItem(R.string.treat_perspective_paragraph_3),
+        HeaderItem(R.string.generic_notification_header_3),
+        BulletedListItem(R.string.post_notification_list_4),
         HeaderItem(R.string.post_notification_header_6),
         ParagraphItem(R.string.post_notification_paragraph_7),
         ParagraphItem(R.string.post_notification_paragraph_8),
         ParagraphItem(R.string.post_notification_paragraph_9, phoneNumber),
         MessageBoxItem(R.string.post_notification_message),
-        ButtonItem(R.string.post_notification_button, onCallClicked)
+        HeaderItem(R.string.treat_perspective_header_1),
+        BulletedListItem(R.string.treat_perspective_list_2)
     )
 )
