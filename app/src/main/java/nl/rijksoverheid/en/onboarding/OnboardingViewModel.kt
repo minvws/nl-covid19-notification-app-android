1ae860e37a948afae6dd394a89a22c0a1a6e17d7
Jolanda Verhoef <jolanda.verhoef@bdmcc.net>
2020-07-19 09:37:19 +0200

    Add confirmation dialog when user wants to skip enabling the API in onboarding

--- app/src/main/java/nl/rijksoverheid/en/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/nl/rijksoverheid/en/onboarding/OnboardingViewModel.kt
@@ -13,11 +13,16 @@ import nl.rijksoverheid.en.lifecyle.Event
 
 class OnboardingViewModel(private val repository: OnboardingRepository) : ViewModel() {
     val onboardingComplete: LiveData<Event<Unit>> = MutableLiveData()
+    val skipConsentConfirmation: LiveData<Event<Unit>> = MutableLiveData()
 
     fun isGooglePlayServicesUpToDate() = repository.isGooglePlayServicesUpToDate()
 
     fun finishOnboarding() {
         repository.setHasCompletedOnboarding(true)
         (onboardingComplete as MutableLiveData).value = Event(Unit)
+    }
+
+    fun skipConsent() {
+        (skipConsentConfirmation as MutableLiveData).value = Event(Unit)
     }
 }
---
+++ app/src/main/java/nl/rijksoverheid/en/onboarding/SkipConsentConfirmationDialogFragment.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+package nl.rijksoverheid.en.onboarding
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import nl.rijksoverheid.en.R
+
+class SkipConsentConfirmationDialogFragment : DialogFragment() {
+
+    companion object {
+        const val SKIP_CONSENT_RESULT = "skip_consent"
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = MaterialAlertDialogBuilder(context)
+            .setTitle(R.string.onboarding_consent_skip_dialog_title)
+            .setMessage(R.string.onboarding_consent_skip_dialog_message)
+            .setPositiveButton(R.string.onboarding_consent_skip_dialog_enable) { _, _ ->
+                findNavController().currentBackStackEntry?.savedStateHandle?.set(
+                    SKIP_CONSENT_RESULT, false
+                )
+            }
+            .setNegativeButton(R.string.onboarding_consent_skip_dialog_skip) { _, _ ->
+                findNavController().currentBackStackEntry?.savedStateHandle?.set(
+                    SKIP_CONSENT_RESULT, true
+                )
+            }
+        return builder.create()
+    }
+}
