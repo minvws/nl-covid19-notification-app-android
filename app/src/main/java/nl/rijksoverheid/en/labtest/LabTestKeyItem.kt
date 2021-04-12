054674d97e090fe8d93c895f3f50001cbf447225
Erik Hoogendoorn <erik.hoogendoorn@gmail.com>
2021-04-12 14:50:18 +0200

    Add lab test key formatting to LabTestKeyItem.

--- app/src/main/java/nl/rijksoverheid/en/labtest/LabTestKeyItem.kt
+++ app/src/main/java/nl/rijksoverheid/en/labtest/LabTestKeyItem.kt
@@ -22,6 +22,9 @@ class LabTestKeyItem(private val keyState: KeyState, private val copy: (String) 
         val key: String? = null,
         val retry: () -> Unit
     ) {
+        val keyPart1 = key?.substring(0..2)
+        val keyPart2 = key?.substring(3..4)
+        val keyPart3 = key?.substring(5..6)
         val keyContentDescription = key?.toLowerCase(Locale.ROOT)?.replace("-", "")
     }
 
