e4add794025e1f71e7124a2ff1653aaa9a63916a
luciano pappalettera <l.pappalettera@dictu.nl>
2021-06-21 13:20:18 +0200

    Show framework disabled item above share keys step

--- app/src/main/java/nl/rijksoverheid/en/labtest/coronatest/CoronaTestKeySharingSection.kt
+++ app/src/main/java/nl/rijksoverheid/en/labtest/coronatest/CoronaTestKeySharingSection.kt
@@ -67,7 +67,7 @@ class CoronaTestKeySharingSection(
             ).apply {
                 if (notificationsState is NotificationsState.Disabled || notificationsState is NotificationsState.Unavailable) {
                     add(
-                        size - 1, // add box as second to last element
+                        2, // add box before the key sharing step
                         ErrorBoxItem(
                             R.string.error_upload_not_available,
                             R.string.status_en_api_disabled_enable,
