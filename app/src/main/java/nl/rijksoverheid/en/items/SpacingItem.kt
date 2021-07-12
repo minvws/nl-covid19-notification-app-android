c0fed52ed0684193b43f39baf54603c791f25207
luciano pappalettera <l.pappalettera@dictu.nl>
2021-07-12 17:20:42 +0200

    Fixed spacing before link

--- app/src/main/java/nl/rijksoverheid/en/items/SpacingItem.kt
+++ app/src/main/java/nl/rijksoverheid/en/items/SpacingItem.kt
@@ -1,11 +1,9 @@
 /*
- *  Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
- *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
  *
- *   SPDX-License-Identifier: EUPL-1.2
- *
+ *  SPDX-License-Identifier: EUPL-1.2
  */
-
 package nl.rijksoverheid.en.items
 
 import com.xwray.groupie.Item
@@ -15,7 +13,7 @@ import nl.rijksoverheid.en.databinding.ItemSpacingBinding
 object SpacingItem : BaseBindableItem<ItemSpacingBinding>() {
     override fun getLayout() = R.layout.item_spacing
     override fun bind(viewBinding: ItemSpacingBinding, position: Int) {
-       // Nothing to bind
+        // Nothing to bind
     }
 
     override fun isSameAs(other: Item<*>): Boolean = other is SpacingItem
