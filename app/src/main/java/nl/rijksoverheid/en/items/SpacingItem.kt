6ea9bb4b15ac0166fdae15841b030787a02ba47b
luciano pappalettera <l.pappalettera@dictu.nl>
2021-07-12 16:58:54 +0200

    Added extra spacing in key sharing options screen when in portrait

---
+++ app/src/main/java/nl/rijksoverheid/en/items/SpacingItem.kt
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+
+package nl.rijksoverheid.en.items
+
+import com.xwray.groupie.Item
+import nl.rijksoverheid.en.R
+import nl.rijksoverheid.en.databinding.ItemSpacingBinding
+
+object SpacingItem : BaseBindableItem<ItemSpacingBinding>() {
+    override fun getLayout() = R.layout.item_spacing
+    override fun bind(viewBinding: ItemSpacingBinding, position: Int) {
+       // Nothing to bind
+    }
+
+    override fun isSameAs(other: Item<*>): Boolean = other is SpacingItem
+    override fun hasSameContentAs(other: Item<*>) = other is SpacingItem
+}
--- app/src/main/java/nl/rijksoverheid/en/labtest/KeyShareOptionsSection.kt
+++ app/src/main/java/nl/rijksoverheid/en/labtest/KeyShareOptionsSection.kt
@@ -10,11 +10,13 @@ import com.xwray.groupie.Section
 import nl.rijksoverheid.en.R
 import nl.rijksoverheid.en.items.HeaderItem
 import nl.rijksoverheid.en.items.ParagraphItem
+import nl.rijksoverheid.en.items.SpacingItem
 
 class KeyShareOptionsSection : Section(
     listOf(
         HeaderItem(R.string.post_keys_options_header),
         ParagraphItem(R.string.post_keys_options_description),
+        SpacingItem,
         KeyShareOptionItem.CoronaTest,
         KeyShareOptionItem.GGD
     )
